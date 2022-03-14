ce9a98a45b566ed5b57127937bd13852e924dd51
Amanda <mandathompson@yahoo.com>
2022-03-14 11:30:29 -0400

    beginning user routes

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,13 +1,20 @@
 // Schema 
 const mongoose = require('mongoose')
 
-const goalSchema = mongoose.Schema({
-    text: {
-        type: String,
-        requires: [true, 'Please add a text value']
-    }
-}, {
-    timestamps: true,
-})
+const goalSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            requires: true, 
+            // Which model does this ID pertain to:
+            ref: 'User',
+        },
+        text: {
+            type: String,
+            requires: [true, 'Please add a text value']
+        }
+    }, {
+        timestamps: true,
+    })
 
 module.exports = mongoose.model('GoalSchema', goalSchema)
