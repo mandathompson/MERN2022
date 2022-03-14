239cf06e7e0b008ade70af5c082e36ec9a407a99
Amanda <mandathompson@yahoo.com>
2022-03-14 09:50:09 -0400

    adding routes and testing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("whats crackalackin")
+const express = require('express') 
+// allows use of .env file
+const dotenv = require('dotenv').config()
+// take port from .env file OR 5000:
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
