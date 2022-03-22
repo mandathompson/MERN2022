1238118c81e2bceaf402546946f62ece7788d769
Amanda <mandathompson@yahoo.com>
2022-03-22 13:17:24 -0400

    link to repo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="footer">&copy; Amanda Thompson 2022</div>
+    <div className="footer">&copy;<a rel="noreferrer" href="https://github.com/mandathompson/MERN2022" target="_blank"> Amanda Thompson 2022</a></div>
   )
 }
 
